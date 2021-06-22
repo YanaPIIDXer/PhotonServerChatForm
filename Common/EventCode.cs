ef9330c37e1644d9674f519edf288232593a51a0
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-23 01:29:49 +0900

    イベントコード、オペレーションコード定義

---
+++ Common/EventCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+	/// <summary>
+	/// イベントコード
+	/// </summary>
+	public static class EventCode
+	{
+		/// <summary>
+		/// メッセージ受信
+		/// </summary>
+		public static readonly byte RecvMessage = 1;
+	}
+}
---
+++ Common/OperationCode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common
+{
+	/// <summary>
+	/// オペレーションコード
+	/// </summary>
+	public static class OperationCode
+	{
+		/// <summary>
+		/// ログイン
+		/// </summary>
+		public static readonly byte LogIn = 1;
+
+		/// <summary>
+		/// メッセージ送信
+		/// </summary>
+		public static readonly byte SendMessgage = 2;
+	}
+}
